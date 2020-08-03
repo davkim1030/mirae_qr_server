8e0a987eb19168c7e4072e77c93fc12cd07273f4
davkim1030 <davkim1030@gmail.com>
2020-08-03 20:30:02 +0900

    initial commit

---
+++ src/test/java/kr/ac/yonsei/selfcheck/SelfCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.yonsei.selfcheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfCheckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
