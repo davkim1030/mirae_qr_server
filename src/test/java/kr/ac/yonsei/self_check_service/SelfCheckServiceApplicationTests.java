dfb564f86c7f91cf242f577b19d6272d3800f48e
davkim1030 <davkim1030@gmail.com>
2020-08-06 01:21:41 +0900

    Create spring default files

---
+++ src/test/java/kr/ac/yonsei/self_check_service/SelfCheckServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.yonsei.self_check_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfCheckServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
